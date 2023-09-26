10ae68728a962d3379112e4db035a93d03a89486
Naveenbalajivetrivel <12118naveenbalajiv@gmail.com>
2023-09-26 14:46:58 +0530

    Revert "Add files via upload"
    This reverts commit a0d87d4d120abce0a42698015718ba8d269a95a6.

--- index.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    const userType = document.querySelector('input[name="usertype"]:checked').value;
-    
-    if (userType === "customer") {
-        window.location.href = "customer.html"; // Redirect to customer page
-    } else if (userType === "manufacturer") {
-        window.location.href = "bussiness.html"; // Redirect to manufacturer page
-    }
-}
